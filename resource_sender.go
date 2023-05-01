7c608d29b64254ab088616a38063100cd0ec9785
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-05-01 22:42:21 +0300

    fix: Uses req-response instead of stream

--- resource_sender.go
+++ resource_sender.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"google.golang.org/grpc/metadata"
 	"io"
 
 	"github.com/kaytu-io/kaytu-aws-describer/proto/src/golang"
@@ -12,7 +13,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/oauth"
-	"google.golang.org/grpc/metadata"
 )
 
 type ResourceSender struct {
@@ -22,10 +22,10 @@ type ResourceSender struct {
 	resourceChannel  chan *golang.AWSResource
 	resourceIDs      []string
 	doneChannel      chan interface{}
-	stream           golang.DescribeService_DeliverAWSResourcesClient
 	conn             *grpc.ClientConn
 	describeEndpoint string
 	jobID            uint
+	client           golang.DescribeServiceClient
 }
 
 func NewResourceSender(workspaceName string, describeEndpoint string, describeToken string, jobID uint, logger *zap.Logger) (*ResourceSender, error) {
@@ -36,7 +36,6 @@ func NewResourceSender(workspaceName string, describeEndpoint string, describeTo
 		resourceChannel:  make(chan *golang.AWSResource, 1000),
 		resourceIDs:      nil,
 		doneChannel:      make(chan interface{}),
-		stream:           nil,
 		conn:             nil,
 		describeEndpoint: describeEndpoint,
 		jobID:            jobID,
@@ -65,16 +64,7 @@ func (s *ResourceSender) Connect() error {
 	s.conn = conn
 
 	client := golang.NewDescribeServiceClient(conn)
-
-	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.New(map[string]string{
-		"workspace-name":  s.workspaceName,
-		"resource-job-id": fmt.Sprintf("%d", s.jobID),
-	}))
-	stream, err := client.DeliverAWSResources(grpcCtx)
-	if err != nil {
-		return err
-	}
-	s.stream = stream
+	s.client = client
 	return nil
 }
 
@@ -86,7 +76,12 @@ func (s *ResourceSender) ResourceHandler() {
 		}
 
 		s.resourceIDs = append(s.resourceIDs, resource.UniqueId)
-		err := s.stream.Send(resource)
+
+		grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.New(map[string]string{
+			"workspace-name":  s.workspaceName,
+			"resource-job-id": fmt.Sprintf("%d", s.jobID),
+		}))
+		_, err := s.client.DeliverAWSResources(grpcCtx, resource)
 		if err != nil {
 			s.logger.Error("failed to send resource", zap.Error(err))
 			if errors.Is(err, io.EOF) {
@@ -105,7 +100,6 @@ func (s *ResourceSender) ResourceHandler() {
 func (s *ResourceSender) Finish() {
 	s.resourceChannel <- nil
 	_ = <-s.doneChannel
-	s.stream.CloseAndRecv()
 	s.conn.Close()
 }
 
