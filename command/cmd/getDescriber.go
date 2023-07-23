93f053a4d2e3911ec43c646260b8d191958c57f1
saber khazaei <saberk@kaytu.io>
2023-07-23 18:09:38 +0400

    fix: add test for getDescribers and fix some getDescriber problem

---
+++ command/cmd/getDescriber.go
@@ -0,0 +1,63 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/kaytu-io/kaytu-aws-describer/aws"
+	"github.com/kaytu-io/kaytu-util/pkg/describe/enums"
+	"github.com/spf13/cobra"
+)
+
+var (
+	id, name, arn string
+)
+
+// getDescriberCmd represents the getDescriber command
+var getDescriberCmd = &cobra.Command{
+	Use:   "getDescriber",
+	Short: "A brief description of your command",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fields := map[string]string{
+			"id":   id,
+			"name": name,
+			"arn":  arn,
+		}
+
+		output, err := aws.GetSingleResource(
+			context.Background(),
+			resourceType,
+			enums.DescribeTriggerTypeManual,
+			"",
+			nil,
+			"",
+			"",
+			"",
+			"",
+			nil,
+			false,
+			fields,
+		)
+		if err != nil {
+			return fmt.Errorf("AWS: %w", err)
+		}
+
+		js, err := json.Marshal(output)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(string(js))
+		return nil
+	},
+}
+
+func init() {
+	getDescriberCmd.Flags().StringVar(&id, "id", "", "id")
+	getDescriberCmd.Flags().StringVar(&name, "name", "", "name")
+	getDescriberCmd.Flags().StringVar(&arn, "arn", "", "arn")
+	getDescriberCmd.Flags().StringVar(&resourceType, "resourceType", "", "resourceType")
+}
