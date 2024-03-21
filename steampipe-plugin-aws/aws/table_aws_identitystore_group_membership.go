d4109290a2b3c8226ef9552b2f17dd837643030f
artaasadi <arta-c@kaytu.io>
2024-03-21 17:26:34 +0100

    fix: fix identitystore group membership

--- steampipe-plugin-aws/aws/table_aws_identitystore_group_membership.go
+++ steampipe-plugin-aws/aws/table_aws_identitystore_group_membership.go
@@ -2,7 +2,6 @@ package aws
 
 import (
 	"context"
-	"github.com/aws/aws-sdk-go-v2/service/identitystore"
 	identitystorev1 "github.com/aws/aws-sdk-go/service/identitystore"
 	"github.com/kaytu-io/kaytu-aws-describer/pkg/kaytu-es-sdk"
 
@@ -27,25 +26,25 @@ func tableAwsIdentityStoreGroupMembership(_ context.Context) *plugin.Table {
 				Name:        "membership_id",
 				Description: "The identifier for a GroupMembership object in an identity store.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("Description.GroupMembership.MembershipId"),
+				Transform:   transform.FromField("Description.MembershipId"),
 			},
 			{
 				Name:        "identity_store_id",
 				Description: "The globally unique identifier for the identity store.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("Description.GroupMembership.IdentityStoreId"),
+				Transform:   transform.FromField("Description.IdentityStoreId"),
 			},
 			{
 				Name:        "group_id",
 				Description: "The identifier for a group in the identity store.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("Description.GroupMembership.GroupId"),
+				Transform:   transform.FromField("Description.GroupId"),
 			},
 			{
 				Name:        "member_id",
 				Description: "Specific identifier for a user indicates that the user is a member of the group.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("Description.GroupMembership.MemberId.Value"),
+				Transform:   transform.FromField("Description.MemberId.Value"),
 			},
 
 			// Standard columns for all tables
@@ -53,51 +52,8 @@ func tableAwsIdentityStoreGroupMembership(_ context.Context) *plugin.Table {
 				Name:        "title",
 				Description: resourceInterfaceDescription("title"),
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("Description.GroupMembership.MembershipId"),
+				Transform:   transform.FromField("Description.MembershipId"),
 			},
 		}),
 	}
-}
-
-//// LIST FUNCTION
-
-func listIdentityStoreGroupMemberships(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	// Create Session
-	svc, err := IdentityStoreClient(ctx, d)
-	if err != nil {
-		plugin.Logger(ctx).Error("aws_identitystore_group_membership.listIdentityStoreGroupMemberships", "get_client_error", err)
-		return nil, err
-	}
-
-	// Limiting the results
-	maxLimit := int32(50)
-	if d.QueryContext.Limit != nil {
-		limit := int32(*d.QueryContext.Limit)
-		if limit < maxLimit {
-			maxLimit = limit
-		}
-	}
-
-	params := &identitystore.ListGroupMembershipsInput{}
-
-	paginator := identitystore.NewListGroupMembershipsPaginator(svc, params, func(o *identitystore.ListGroupMembershipsPaginatorOptions) {
-		o.StopOnDuplicateToken = true
-	})
-
-	for paginator.HasMorePages() {
-		output, err := paginator.NextPage(ctx)
-		if err != nil {
-			plugin.Logger(ctx).Error("aws_identitystore_group_membership.listIdentityStoreGroupMemberships", "api_error", err)
-			return nil, err
-		}
-		for _, item := range output.GroupMemberships {
-			d.StreamListItem(ctx, item)
-			// Context may get cancelled due to manual cancellation or if the limit has been reached
-			if d.RowsRemaining(ctx) == 0 {
-				return nil, nil
-			}
-		}
-	}
-
-	return nil, nil
 }
