02c24fd99a91d6d2a346795ece5a0ff0b5a2bf81
artaasadi <arta-c@kaytu.io>
2024-04-08 14:39:14 +0200

    fix: aws_ec2_instance_metric_cpu_utilization_hourly table columns

--- steampipe-plugin-aws/aws/table_aws_ec2_instance_metric_cpu_utilization_hourly.go
+++ steampipe-plugin-aws/aws/table_aws_ec2_instance_metric_cpu_utilization_hourly.go
@@ -19,56 +19,55 @@ func tableAwsEc2InstanceMetricCpuUtilizationHourly(_ context.Context) *plugin.Ta
 			Hydrate: kaytu.ListEC2InstanceMetricCpuUtilizationHourly,
 		},
 
-		Columns: awsKaytuRegionalColumns(cwMetricColumns(
-			[]*plugin.Column{
-				{
-					Name:        "instance_id",
-					Description: "The ID of the instance.",
-					Type:        proto.ColumnType_STRING,
-					Transform:   transform.FromField("Description.InstanceId"),
-				},
-				{
-					Name:        "timestamp",
-					Description: "Datapoint Timestamp.",
-					Type:        proto.ColumnType_TIMESTAMP,
-					Transform:   transform.FromField("Description.Timestamp"),
-				},
-				{
-					Name:        "average",
-					Description: "Sample Average.",
-					Type:        proto.ColumnType_DOUBLE,
-					Transform:   transform.FromField("Description.Average"),
-				},
-				{
-					Name:        "sum",
-					Description: "Sample Sum.",
-					Type:        proto.ColumnType_DOUBLE,
-					Transform:   transform.FromField("Description.Sum"),
-				},
-				{
-					Name:        "maximum",
-					Description: "Sample Maximum.",
-					Type:        proto.ColumnType_DOUBLE,
-					Transform:   transform.FromField("Description.Maximum"),
-				},
-				{
-					Name:        "minimum",
-					Description: "Sample Minimum.",
-					Type:        proto.ColumnType_DOUBLE,
-					Transform:   transform.FromField("Description.Minimum"),
-				},
-				{
-					Name:        "sample_count",
-					Description: "Sample Count.",
-					Type:        proto.ColumnType_DOUBLE,
-					Transform:   transform.FromField("Description.SampleCount"),
-				},
-				{
-					Name:        "account_id",
-					Description: "Account Id of the resource",
-					Type:        proto.ColumnType_STRING,
-					Transform:   transform.FromField("Account"),
-				},
-			})),
+		Columns: awsKaytuColumns([]*plugin.Column{
+			{
+				Name:        "instance_id",
+				Description: "The ID of the instance.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.InstanceId"),
+			},
+			{
+				Name:        "timestamp",
+				Description: "Datapoint Timestamp.",
+				Type:        proto.ColumnType_TIMESTAMP,
+				Transform:   transform.FromField("Description.Timestamp"),
+			},
+			{
+				Name:        "average",
+				Description: "Sample Average.",
+				Type:        proto.ColumnType_DOUBLE,
+				Transform:   transform.FromField("Description.Average"),
+			},
+			{
+				Name:        "sum",
+				Description: "Sample Sum.",
+				Type:        proto.ColumnType_DOUBLE,
+				Transform:   transform.FromField("Description.Sum"),
+			},
+			{
+				Name:        "maximum",
+				Description: "Sample Maximum.",
+				Type:        proto.ColumnType_DOUBLE,
+				Transform:   transform.FromField("Description.Maximum"),
+			},
+			{
+				Name:        "minimum",
+				Description: "Sample Minimum.",
+				Type:        proto.ColumnType_DOUBLE,
+				Transform:   transform.FromField("Description.Minimum"),
+			},
+			{
+				Name:        "sample_count",
+				Description: "Sample Count.",
+				Type:        proto.ColumnType_DOUBLE,
+				Transform:   transform.FromField("Description.SampleCount"),
+			},
+			{
+				Name:        "account_id",
+				Description: "Account Id of the resource",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Account"),
+			},
+		}),
 	}
 }
