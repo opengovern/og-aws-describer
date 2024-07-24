ced687cc2a5b483ea0c6611437b22fc4541bc1d2
Mahan Zendedel DH <mahanz@kaytu.io>
2024-07-24 21:44:43 +0400

    fix reduce queue size (#93)
    * fix: reduce queue size
    
    * fix: topic name

--- local/service.go
+++ local/service.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	StreamName    = "kaytu_aws_describer"
-	JobQueueTopic = "job_queue"
+	JobQueueTopic = "kaytu_aws_describer_job_queue"
 	ConsumerGroup = "aws-describer"
 )
 
@@ -77,7 +77,7 @@ func NewWorker(
 		return nil, err
 	}
 
-	if err := jq.Stream(ctx, StreamName, "aws describe job runner queue", []string{JobQueueTopic}, 1000000); err != nil {
+	if err := jq.Stream(ctx, StreamName, "aws describe job runner queue", []string{JobQueueTopic}, 200000); err != nil {
 		return nil, err
 	}
 
